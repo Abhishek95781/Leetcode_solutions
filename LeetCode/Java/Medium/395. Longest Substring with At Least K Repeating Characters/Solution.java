efe9cd15b68d8eed203474ce01fe609492eae106
Abhishek95781 <nigamabhishek288@gmail.com>
2026-09-15 01:21:13 +0530

    Add Medium Java Solution for 395. Longest Substring with At Least K Repeating Characters from LeetCode

---
+++ LeetCode/Java/Medium/395. Longest Substring with At Least K Repeating Characters/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int longestSubstring(String s, int k) {
+        return solve(s, 0, s.length() - 1, k);
+    }
+
+    private int solve(String s, int left, int right, int k) {
+        if (right - left + 1 < k) {
+            return 0;
+        }
+
+        int[] freq = new int[26];
+
+        for (int i = left; i <= right; i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = left; i <= right; i++) {
+            if (freq[s.charAt(i) - 'a'] < k) {
+                int next = i + 1;
+
+                while (next <= right && freq[s.charAt(next) - 'a'] < k) {
+                    next++;
+                }
+
+                return Math.max(
+                    solve(s, left, i - 1, k),
+                    solve(s, next, right, k)
+                );
+            }
+        }
+
+        return right - left + 1;
+    }
+}
